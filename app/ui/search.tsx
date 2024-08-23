c315776f735ee0c03d0dcfd78eba26d8850c4400
Shashank <shashank.b@codecraft.co.in>
2024-08-23 10:56:54 +0530

    Added Debouncing feature to reduce the number of request sent at every keystroke and Pagination added with URL search parameters instead of client state.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 import { tree } from "next/dist/build/templates/app-page";
 
 export default function Search({ placeholder }: { placeholder: string }) {
@@ -9,19 +10,18 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathName = usePathname();
   const { replace } = useRouter();
   console.log(searchParams);
-  function handleSearch(term: string) {
-    console.log(term);
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
-    console.log(params);
+    params.set("page", "1");
     if (term) {
       params.set("query", term);
     } else {
       params.delete("query");
     }
 
-    console.log(params);
     replace(`${pathName}?${params.toString()}`);
-  }
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -33,7 +33,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
